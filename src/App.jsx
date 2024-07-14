35ac134cafca049837b2ebb2659f8a236ae9b4be
AmshShrestha <sthaamsh37@gmail.com>
2024-07-14 09:47:38 +0545

    Repository Created

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>This is where we render page</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navigationbar/NavigationBar.jsx
@@ -0,0 +1,11 @@
+const NavigationBar = () => {
+  return (
+    <nav>
+      <div>
+        <h1>NavigationBar</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
